f13b3b065fe36470f4a7badf8a809f3e1ea384d5
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-10 22:34:37 +0600

    add comment user and user comments solve

--- js/app.js
+++ js/app.js
@@ -51,9 +51,9 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post);
   const image = post.image;
   const profilePic = post.userImage;
+  const comment = post.comments[0];
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${comment.user}
                       </a>
-                      ${post.comments?.text}
+                      ${comment.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
